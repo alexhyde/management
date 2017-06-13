563e25bffbb74baefa937c8d422a529ffa6aa109
Alex Hyde <alex.hyde101@gmail.com>
2017-06-13 01:00:59 -0400

    Added angular 2 front end

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,63 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var tsc = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+var sourcemaps = require('gulp-sourcemaps');
+var tsProject = tsc.createProject('src/tsconfig.json');
 
 var jsFiles = ['*.js','src/**/*.js'];
+
+var config = {
+		allTs: './src/**/*.ts',
+		typings: './src/typings/**/*.d.ts',
+		tsOutputPath: './src/'
+	};
 
 gulp.task('style',function(){
 	return gulp.src(jsFiles).pipe(jshint());
 });
 
-gulp.task('inject',function(){
-		var wiredep = require('wiredep').stream;
-		var inject = require('gulp-inject');
-		
-		var injectSrc = gulp.src(['./public/css/*.css','./public/js/*.js']);
-		var injectOptions = {ignorePath:'/public'};
-		
-		var options = {bowerJson: require('./bower.json'),directory: './bower_components'};
-		
-		return gulp.src('./src/views/*.html')
-		.pipe(wiredep(options))
-		.pipe(inject(injectSrc, injectOptions))
-		.pipe(gulp.dest('./src/views'));
+gulp.task('ts-lint', function() {
+	return gulp.src(config.allTs)
+		.pipe(tslint())
+		.pipe(tslint.report('prose', {
+			emitError: false
+	    }))
 });
 
-gulp.task('serve',['style','inject'], function(){
-	var options = {script:'app.js',
+gulp.task('compile-ts', function(){
+	var sourceTsFiles = [
+		config.allTs,
+		config.typings
+	];
+		
+	var tsResult = gulp
+		.src(sourceTsFiles)
+		.pipe(sourcemaps.init())
+		.pipe(tsc(tsProject));
+	
+	//var tsResult = tsProject.src() // instead of gulp.src(...)
+   //     .pipe(tsc(tsProject));
+		
+		return tsResult.js
+		    .pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest(config.tsOutputPath));
+			
+});
+
+gulp.task('serveServer',['style'], function(){
+	var options = {script:'server.js',
 	delayTime:1,
 	watch: jsFiles};
 	return nodemon(options).on('restart',function(){
 		console.log('Server Restarting ...');
 	});
 });
+
+gulp.task('serveClient', ['ts-lint', 'compile-ts'], function(){
+	gulp.watch([config.allTs], ['ts-lint', 'compile-ts']);
+	return console.log('TypeScript Recompiling');
+});
+
+gulp.task('default', ['serveServer','serveClient']);
--- src/routes/invRoutes.js
+++
@@ -1,8 +0,0 @@
-var expressImport = require('express');
-var invRouter = expressImport.Router();
-
-invRouter.route('/').get(function(req,res){
-	res.send('Inventory Page');
-});
-
-module.exports=invRouter;
